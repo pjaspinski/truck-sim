9cc9fd01a24a0f2b4713240eb06165b28a44c40f
unknown <pawel.jaspinski@tellyo.com>
2025-02-01 13:38:04 +0100

    Reporting button statuses on every loop

--- gear-knob/gear_knob_range_and_splitter.ino
+++ gear-knob/gear_knob_range_and_splitter.ino
@@ -8,9 +8,6 @@ int SPLIT_PIN = 3;
 int RANGE_BTN_IDX = 0;
 int SPLIT_BTN_IDX = 1;
 
-int rangeState;
-int splitState;
-
 Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID, 
   JOYSTICK_TYPE_GAMEPAD, 2, 0,
   false, false, false, false, false, false,
@@ -27,33 +24,25 @@ void buttonPressRelease(int button_id, int state) {
   }
 }
 
-void setup() {
-  Joystick.begin();
-  pinMode(LED_PIN, OUTPUT);
-
+void readAndPress() {
   // Range
   pinMode(RANGE_PIN, INPUT_PULLUP);
-  rangeState = digitalRead(RANGE_PIN);
+  int rangeState = digitalRead(RANGE_PIN);
   buttonPressRelease(RANGE_BTN_IDX, rangeState);
 
   // Split
   pinMode(SPLIT_PIN, INPUT_PULLUP);
-  splitState = digitalRead(SPLIT_PIN);
+  int splitState = digitalRead(SPLIT_PIN);
   buttonPressRelease(SPLIT_BTN_IDX, splitState);
 }
 
-void loop() {
-  // Range
-  int rangeRead = digitalRead(RANGE_PIN);
-  if(rangeRead != rangeState) {
-    rangeState = rangeRead;
-    buttonPressRelease(RANGE_BTN_IDX, rangeRead);
-  }
+void setup() {
+  Joystick.begin();
+  pinMode(LED_PIN, OUTPUT);
 
-  // Split
-  int splitRead = digitalRead(SPLIT_PIN);
-  if(splitRead != splitState) {
-    splitState = splitRead;
-    buttonPressRelease(SPLIT_BTN_IDX, splitRead);
-  }
+  readAndPress();
+}
+
+void loop() {
+  readAndPress();
 }
